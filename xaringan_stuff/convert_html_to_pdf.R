9f23e6df1fd6338e061d502d014634b2d1ad8665
jannes-m <malnamalja@gmx.de>
2018-08-14 19:20:33 +0200

    adjust convert_html_to_pdf

--- xaringan_stuff/convert_html_to_pdf.R
+++ xaringan_stuff/convert_html_to_pdf.R
@@ -8,14 +8,24 @@ browseURL("https://github.com/astefanutti/decktape/releases/tag/v1.0.0")
 # 3. execute ./phantomjs decktape.js -h
 
 
-dir_deck = "pres/rmd/decktape-1.0.0"
-files = dir("pres/rmd", pattern = ".html$")
+files = dir("pres/", recursive = TRUE, pattern = ".html$")
+files = files[1]
 for (i in files) {
-  in_file = file.path("pres/rmd", i)
-  out_file = file.path("pres/pdf", gsub(".html", ".pdf", i))
-  cmd = paste(file.path(dir_deck, "phantomjs"), 
-              file.path(dir_deck, "decktape.js"),
-              in_file, out_file)
-  system(cmd)
+  in_file = file.path("pres", i)
+  out_file = file.path("pres", gsub(".html", ".pdf", i))
+  webshot(in_file, out_file)
+
 }
+
+# Windows style
+# dir_deck = "xaringan_stuff/decktape-1.0.0"
+# files = dir("pres/", recursive = TRUE, pattern = ".html$")
+# for (i in files) {
+#   in_file = file.path("pres", i)
+#   out_file = file.path("pres", gsub(".html", ".pdf", i))
+#   cmd = paste(file.path(dir_deck, "phantomjs"),
+#               file.path(dir_deck, "decktape.js"),
+#               in_file, out_file)
+#   system(cmd)
+# }
 
